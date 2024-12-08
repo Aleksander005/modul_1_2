25730cb475697b48702d077587508448dcdbf1a2
Aleksander005 <dmitrijdavydov853@gmail.com>
2024-12-08 09:46:18 +0300

    Add files via upload

---
+++ modul_1_2.py
@@ -0,0 +1,10 @@
+home_work_lessons = 12
+home_work_hour = 1.5
+mame_course = "Python"
+time_on_lesson = (home_work_hour / home_work_lessons )
+print(time_on_lesson)
+#
+# Напишите 4 переменных которые буду обозначать следующие данные:
+#
+# Количество выполненных ДЗ (запишите значение 12)
+# Количес
