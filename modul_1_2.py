f4bfe7f7e69b5fd0d579a6cb969d41e9e80c902b
Aleksander005 <dmitrijdavydov853@gmail.com>
2024-12-08 09:49:54 +0300

    Update modul_1_2.py

--- modul_1_2.py
+++ modul_1_2.py
@@ -1,10 +1,7 @@
+mame_course = "Python"
 home_work_lessons = 12
 home_work_hour = 1.5
-mame_course = "Python"
-time_on_lesson = (home_work_hour / home_work_lessons )
-print(time_on_lesson)
-#
-# Напишите 4 переменных которые буду обозначать следующие данные:
-#
-# Количество выполненных ДЗ (запишите значение 12)
-# Количес
+time_on_lesson = (home_work_hour / home_work_lessons)
+print('Курс:', mame_course, ',' 'всего задач:'
+      , home_work_lessons, 'затрачено часов:', home_work_hour,
+'среднее время выполнения:', time_on_lesson )
